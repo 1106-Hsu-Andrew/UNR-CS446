673ca39b52b8dae28df47f3b8ddcf6172e1f1657
1106-Hsu-Andrew <andrewhsu62@gmail.com>
2026-02-12 13:56:03 -0800

    hw2

---
+++ PA2_Hsu_Andrew/threaded_sum.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int readFile(char[], int[]);
+void* arraySum(void*);
+
+int readFile(char fileName[], int fileContents[]);{
+    FILE fptr;
+
+    fptr = fopen(fileName, "r");
+    
+}
+
+void* arraySum(void* inputData){
+
+}
+
+int main(){
+
+}
