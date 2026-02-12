2210324888d0e3bbfe229b79361b2fc1322945aa
1106-Hsu-Andrew <andrewhsu62@gmail.com>
2026-02-12 14:00:42 -0800

    finished readFile function

--- PA2_Hsu_Andrew/threaded_sum.c
+++ PA2_Hsu_Andrew/threaded_sum.c
@@ -4,9 +4,20 @@ int readFile(char[], int[]);
 void* arraySum(void*);
 
 int readFile(char fileName[], int fileContents[]);{
+    int successfulReads = 0;
     FILE fptr;
 
     fptr = fopen(fileName, "r");
+    if(fptr == NULL){
+        printf("File not found...");
+        return -1;
+    }
+    
+    while(fscanf(fptr, "%d", fileContents) == 1){
+        successfulReads++;
+    }
+    fclose();
+    return successfulReads;
     
 }
 
