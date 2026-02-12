c7961f1362580a0cf34a129afd627e7d04d4b4d1
1106-Hsu-Andrew <andrewhsu62@gmail.com>
2026-02-12 14:25:36 -0800

    added thread_data_t struct and associated libraries

--- PA2_Hsu_Andrew/threaded_sum.c
+++ PA2_Hsu_Andrew/threaded_sum.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <string.h>
+#include <pthread.h>
+#include <sys/time.h>
 
 int readFile(char[], int[]);
 void* arraySum(void*);
+
+typedef struct _thread_data_t{
+    const int* data;
+    int startInd;
+    int endInd;
+    pthread_mutex_t* lock;
+    long long int* totalSum;
+} thread_data_t;
 
 int readFile(char fileName[], int fileContents[]){
     int successfulReads = 0;
@@ -27,15 +37,16 @@ void* arraySum(void* inputPtr){
 }
 
 int main(int argc, char* argv[]){
+    int fileContents[1000000];
+
     if(argc != 3){
         printf("There aren't enough parameters.");
         return -1;
     }
 
-    int fileContents[1000000];
-
     int fileLen = readFile(argv[1], fileContents);
-    for(int i = 0; i < fileLen; i++){
-        printf("%d\n", fileContents[i]);
+    if(argv[2] > fileLen){
+        printf("Too many threads requested.");
+        return -1;
     }
 }
