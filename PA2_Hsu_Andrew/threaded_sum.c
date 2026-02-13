7bf6a70be418d4e9b94f82aafa80c79af269485a
1106-Hsu-Andrew <andrewhsu62@gmail.com>
2026-02-12 17:27:09 -0800

    fixed the totalSum bug

--- PA2_Hsu_Andrew/threaded_sum.c
+++ PA2_Hsu_Andrew/threaded_sum.c
@@ -18,12 +18,12 @@ typedef struct _thread_data_t{
 void* arraySum(void* inputPtr){
     thread_data_t* threadPtr = (thread_data_t*)inputPtr;
 
-    pthread_mutex_lock(threadPtr->lock);
     long long int threadSum = 0;
     for(int i = threadPtr->startInd; i < threadPtr->endInd; i++){
         threadSum += threadPtr->data[i];
     }
 
+    pthread_mutex_lock(threadPtr->lock);
     *(threadPtr->totalSum) += threadSum;
     pthread_mutex_unlock(threadPtr->lock);
 }
@@ -34,7 +34,7 @@ int readFile(char fileName[], int fileContents[]){
 
     fptr = fopen(fileName, "r");
     if(fptr == NULL){
-        printf("File not found...");
+        printf("File not found...\n");
         return -1;
     }
     
@@ -54,13 +54,13 @@ int main(int argc, char* argv[]){
     int totalThreads = atoi(argv[2]);
 
     if(argc != 3){
-        printf("There aren't enough parameters.");
+        printf("There aren't enough parameters.\n");
         return -1;
     }
     int fileLen = readFile(argv[1], fileContents);
 
     if(totalThreads > fileLen){
-        printf("Too many threads requested.");
+        printf("Too many threads requested.\n");
         return -1;
     }
     gettimeofday(&startTime, NULL);
@@ -70,7 +70,13 @@ int main(int argc, char* argv[]){
     for(int i = 0; i < totalThreads; i++){
         threadData[i].data = fileContents;
         threadData[i].startInd = (fileLen / totalThreads) * i;
-        threadData[i].endInd = (fileLen / totalThreads) * (i + 1);
+
+        if(i == totalThreads - i){
+            threadData[i].endInd = fileLen;
+        }
+        else{
+            threadData[i].endInd = (fileLen / totalThreads) * (i + 1);
+        }
         threadData[i].lock = &mutex;
         threadData[i].totalSum = &totalSum;
     }
@@ -81,11 +87,15 @@ int main(int argc, char* argv[]){
     for(int i = 0; i < totalThreads; i++){
         void* voidThreadData = &threadData[i];
         pthread_create(&threadArray[i], NULL, arraySum, voidThreadData);
-        pthread_join(threadArray[i], NULL);
-
-        gettimeofday(&endTime, NULL);
-        elapsedTime = (endTime.tv_sec - startTime.tv_sec) * 1000.0;
-        elapsedTime += (endTime.tv_usec - startTime.tv_usec) / 1000.0;
     }
+
+    for(int i = 0; i < totalThreads; i++){
+        pthread_join(threadArray[i], NULL);
+    }
+    gettimeofday(&endTime, NULL);
+    elapsedTime = (endTime.tv_sec - startTime.tv_sec) * 1000.0;
+    elapsedTime += (endTime.tv_usec - startTime.tv_usec) / 1000.0;
+
     printf("Total Sum: %lld\nTotal Execution Time: %f ms\n", totalSum, elapsedTime);
+    return 0;
 }
