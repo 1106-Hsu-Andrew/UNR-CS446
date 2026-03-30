5be9f332d1f066d328a057f6b69c7852315ec738
1106-Hsu-Andrew <72996868+1106-Hsu-Andrew@users.noreply.github.com>
2026-03-30 16:46:55 -0700

    Implement print_progress function for task visualization
    Added a print_progress function to display progress with color coding.

--- PA4_Hsu_Andrew/sched.c
+++ PA4_Hsu_Andrew/sched.c
@@ -1,15 +1,73 @@
+#include <sys/syscall.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+#include <sys/mman.h>
 #include <pthread.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <time.h>
-#include "print_progress.c"
+
+#define ANSI_COLOR_GRAY    "\x1b[30m"
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_WHITE   "\x1b[37m"
+
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+#define TERM_CLEAR() printf("\033[H\033[J")
+#define TERM_GOTOXY(x,y) printf("\033[%d;%dH", (y), (x))
+
+void print_progress(pid_t localTid, size_t value) {
+        pid_t tid = syscall(__NR_gettid);
+
+        TERM_GOTOXY(0,localTid+1);
+
+	char prefix[256];
+        size_t bound = 100;
+        sprintf(prefix, "%d: %ld (ns) \t[", tid, value);
+	const char suffix[] = "]";
+	const size_t prefix_length = strlen(prefix);
+	const size_t suffix_length = sizeof(suffix) - 1;
+	char *buffer = calloc(bound + prefix_length + suffix_length + 1, 1);
+	size_t i = 0;
+
+	strcpy(buffer, prefix);
+	for (; i < bound; ++i)
+	{
+	    buffer[prefix_length + i] = i < value/10000 ? '#' : ' ';
+	}
+	strcpy(&buffer[prefix_length + i], suffix);
+        
+        if (!(localTid % 7)) 
+            printf(ANSI_COLOR_WHITE "\b%c[2K\r%s\n" ANSI_COLOR_RESET, 27, buffer);  
+        else if (!(localTid % 6)) 
+            printf(ANSI_COLOR_BLUE "\b%c[2K\r%s\n" ANSI_COLOR_RESET, 27, buffer);  
+        else if (!(localTid % 5)) 
+            printf(ANSI_COLOR_RED "\b%c[2K\r%s\n" ANSI_COLOR_RESET, 27, buffer);  
+        else if (!(localTid % 4)) 
+            printf(ANSI_COLOR_GREEN "\b%c[2K\r%s\n" ANSI_COLOR_RESET, 27, buffer);  
+        else if (!(localTid % 3)) 
+            printf(ANSI_COLOR_CYAN "\b%c[2K\r%s\n" ANSI_COLOR_RESET, 27, buffer);  
+        else if (!(localTid % 2)) 
+            printf(ANSI_COLOR_YELLOW "\b%c[2K\r%s\n" ANSI_COLOR_RESET, 27, buffer);  
+        else if (!(localTid % 1)) 
+            printf(ANSI_COLOR_MAGENTA "\b%c[2K\r%s\n" ANSI_COLOR_RESET, 27, buffer);  
+        else
+            printf("\b%c[2K\r%s\n", 27, buffer);
+
+	fflush(stdout);
+	free(buffer);
+}
 
 void* arraySum(void* inputData);
 typedef struct _thread_data_t {
